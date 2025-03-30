9ba8bbb9e8033be53290a5382847716be827a123
Abdullah <abdullahnahk655@gmail.com>
2025-03-30 09:12:09 +0500

    Update Stockviz.py

--- StockViz_Pro/Stockviz.py
+++ StockViz_Pro/Stockviz.py
@@ -16,7 +16,7 @@ st.set_page_config(
 )
 
 # Custom CSS
-with open(StockViz_Pro/'styles.css') as f:
+with open('styles.css') as f: 
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Initialize session state
